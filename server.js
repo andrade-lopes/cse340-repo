f301730103763357f08a95a53cb7384cbbf8f775
andrade-lopes <jlopes@byupathway.edu>
2026-09-05 21:28:35 -0100

    Set up Express EJS and CSS

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -6,10 +8,22 @@ const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
+app.set('view engine', 'ejs');
+
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello from Express! Nodemon is working!');
+    res.render('home');
 });
 
 app.listen(PORT, () => {
